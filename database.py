a4e2abd9a1795e7430db0b5e438624e0cce30399
salemzii <salemododa2@gmail.com>
2023-09-02 19:15:01 +0100

    envr

--- database.py
+++ database.py
@@ -2,7 +2,7 @@ import os
 import motor.motor_asyncio
 from dotenv import load_dotenv
 
-load_dotenv()
+load_dotenv("../.env")
 #os.environ["MONGODB_URL"]
 client = motor.motor_asyncio.AsyncIOMotorClient(os.environ["MONGODB_URL"])
 db = client.wenyfour
